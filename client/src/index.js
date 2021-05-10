112e565be98fcbf9145efbb3a79451e7f0dd22b7
Shreejeeb Kesh <shreejeebkesh@gmail.com>
2021-05-10 15:13:35 +0530

    styling , ui-components and redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,15 @@
 import React from "react";
 import { render } from "react-dom";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { createStore, applyMiddleware, compose } from "redux";
 import App from "./App";
+import reducers from "./redux/reducers";
 
-render(<App />, document.getElementById("root"));
+const store = createStore(reducers, applyMiddleware(compose(thunk)));
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-
+const postsRouter = require("./routes/posts.js");
 const app = express();
 
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use("/posts/", postsRouter);
 
 const CONNECTION_URL =
   "mongodb+srv://srijibkesh:Srijib123@cluster0.emtnp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
