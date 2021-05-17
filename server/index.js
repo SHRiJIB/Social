a7951c87f7370b5b967b24aadbabcccdc17a9779
Shreejeeb <shreejeebkesh@gmail.com>
2021-05-17 21:24:29 +0530

    deploying

--- server/index.js
+++ server/index.js
@@ -10,6 +10,9 @@ app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("Welcome to my social app.");
+});
 app.use("/posts/", postsRouter);
 
 const PORT = process.env.PORT || 5000;
