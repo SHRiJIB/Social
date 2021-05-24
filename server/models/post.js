1eb9bfba9b46e8950eb29e3a62550b1f30fb8f2e
Shreejeeb <shreejeebkesh@gmail.com>
2021-05-24 19:24:54 +0530

    User model and modified posts model

--- server/models/post.js
+++ server/models/post.js
@@ -8,9 +8,9 @@ const postSchema = new schema(
     creator: String,
     tags: [String],
     selectedFile: String,
-    likeCount: {
-      type: Number,
-      default: 0,
+    likes: {
+      type: [String],
+      default: [],
     },
   },
   { timestamps: true }
